f8160290e4da6581de3b76d8e90a74ba8252abaf
ShOwY <showy@bt.foo.org>
2012-10-04 00:49:12 -0430

    Initializing basic functions for the rest of the book

---
+++ lib/error_functions.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include "error_functions.h"
+#include "tlpi_hdr.h"
+#include "ename.c.inc"
+
+#ifdef __GNUC__
+__attribute__ ((__noreturn__))
+#endif
+
+// Functions
+
+static void terminate( Boolean useExit3 ) {
+	char * s;
+
+	s = getenv("EF_DUMPCORE");
+
+	if ( s != NULL && *s != '\0') {
+		abort();
+	} else if (useExit3) {
+		exit(EXIT_FAILURE);
+	} else {
+		_exit(EXIT_FAILURE);
+	}
+	
+}
+
